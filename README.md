269e61aefccff88ea2ff1112882e96c270c1141f
strong-code <colin@strongco.de>
2020-04-18 17:28:39 -0700

    first commit and make.sh

---
+++ README.md
@@ -0,0 +1,5 @@
+# TIL 
+
+[vim](./vim/) 
+
+generated from [make.sh](./make.sh)
---
+++ make.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+rm README.md
+touch README.md
+
+echo $'# TIL \n' >> README.md
+
+for d in */ ; do
+  echo "[${d%/}](./$d)" $'\n' >> README.md 
+done
+
+echo "generated from [make.sh](./make.sh)" >> README.md
